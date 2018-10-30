d8d98e830074846f519f56685b80a27d03163fdb
Marko <salamanka1985@gmail.com>
2018-10-30 12:51:53 +0100

    Add struct Request and createRequest function

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -2,9 +2,22 @@ pragma solidity ^0.4.17;
 
 contract Campaign {
 
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
+    Request[] public requests;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
     
     constructor(uint minimum) public{
         manager = msg.sender;
@@ -14,6 +27,17 @@ contract Campaign {
     function contribute() public payable {
         require(msg.value > minimumContribution);
         approvers.push(msg.sender);
+    }
+
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request memory newRequest = Request({
+           description: description,
+           value: value,
+           recipient: recipient,
+           complete: false
+        }); 
+       
+        requests.push(newRequest);
     }
 
 }
