25bb2dcdf2e4b456d077a86b5c6c980ed92f313f
Marko <salamanka1985@gmail.com>
2018-10-30 12:47:14 +0100

    Add contract manager and contribute function

---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.17;
+
+contract Campaign {
+
+    address public manager;
+    uint public minimumContribution;
+    address[] public approvers;
+    
+    constructor(uint minimum) public{
+        manager = msg.sender;
+        minimumContribution = minimum;
+    }
+    
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        approvers.push(msg.sender);
+    }
+
+}
